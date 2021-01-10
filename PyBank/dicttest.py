3b8896c16c9551ace7c686dbc43ecb1ea62384a5
Susan Chambers <chamberssu@gmail.com>
2021-01-10 14:08:35 -0600

    initial commit

---
+++ PyBank/dicttest.py
@@ -0,0 +1,22 @@
+
+# Python3 code to demonstrate  
+# conversion of lists to dictionary 
+# using naive method 
+  
+# initializing lists 
+test_keys = ["Rash", "Kil", "Varsha"] 
+test_values = [1, 4, 5] 
+  
+# Printing original keys-value lists 
+print ("Original key list is : " + str(test_keys)) 
+print ("Original value list is : " + str(test_values)) 
+  
+# using naive method 
+# to convert lists to dictionary 
+res = {} 
+for key in test_keys: 
+    for value in test_values: 
+        res[key] = value 
+        test_values.remove(value) 
+        break  
+print(res)
