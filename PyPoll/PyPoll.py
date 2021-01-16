a91f8ae6e3cba152d252adbe4125cb44e0bb7d56
Susan Chambers <chamberssu@gmail.com>
2021-01-16 15:07:24 -0600

    Made edits to return election results

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -5,11 +5,12 @@ import operator
 path = r"C:\Users\chamb\Desktop\python-challenge\PyPoll\Resources"
 pypoll_csv = os.path.join(path,'election_data.csv')
 results = {}
+candidates=[]
+khan = 0
+otooley = 0
+li = 0
+correy = 0
 
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0
 
 with open(pypoll_csv) as csvfile:
 
@@ -23,43 +24,30 @@ with open(pypoll_csv) as csvfile:
 
     total_votes = 0
     # Read each row of data after the header
+
     for row in csvreader:
         total_votes = total_votes + 1
-        results = {"Candidate" : }
-        profit_losses.append(int(row[1]))
-        
+        candidates.append(row[2])
+    for x in candidates: 
+        if (x == "Khan"): 
+            khan = khan + 1
+        if (x == "O'Tooley"):
+            otooley = otooley + 1
+        if (x == "Li"):
+            li = li + 1
+        if (x == "Correy"):
+            correy = correy + 1
+results = {"Khan":khan, "Correy":correy, "O'Tooley":otooley, "Li":li}
 
-print(f"Total Votes: {total_votes} Khan Votes: {khan_votes}")
-        
+print("Election Results")
+print("---------------")
+print(f"Total Votes: {total_votes}")
+print("---------------") 
+print(f"Khan: {((khan/total_votes)*100):.3f}% ({khan})")
+print(f"Correy: {((correy/total_votes)*100):.3f}% ({correy})")
+print(f"Li: {((li/total_votes)*100):.3f}% ({li})")
+print(f"O'Tooley: {((otooley/total_votes)*100):.3f}% ({otooley})")
+print(f"Winner: {max(results.items(), key=operator.itemgetter(1))[0]}")
 
-# candidate votes percent and Raw
-# winner
-#         * You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+#https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
 
-#   * The total number of votes cast
-
-#   * A complete list of candidates who received votes
-
-#   * The percentage of votes each candidate won
-
-#   * The total number of votes each candidate won
-
-#   * The winner of the election based on popular vote.
-
-# * As an example, your analysis should look similar to the one below:
-
-#   ```text
-#   Election Results
-#   -------------------------
-#   Total Votes: 3521001
-#   -------------------------
-#   Khan: 63.000% (2218231)
-#   Correy: 20.000% (704200)
-#   Li: 14.000% (492940)
-#   O'Tooley: 3.000% (105630)
-#   -------------------------
-#   Winner: Khan
-#   -------------------------
-#   ```
-
-# * In addition, your final script should both print the analysis to the terminal and export a text file with the results.
